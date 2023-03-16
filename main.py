6683ed3133065347a6618c5b2aa18b09f365a809
Izmar Verhage <izmaelverhage@gmail.com>
2023-03-16 08:23:37 +0100

    Knee 1

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def getEnvelopeAmplitude(attackTime, decayTime, attackIncrement, decayIncrement)
             if (playable_length - indexWithinPlayableLength) < 8:
                 envelopeAmplitude -= 0.125
             else:
-                envelopeAmplitude += - ((1.3 - relative_remainder) * 0.0000125)
+                envelopeAmplitude += - (((1.15 - (relative_remainder * 0.6)) ** 5) * 0.00003)
 
 attack_value = 100
 plot_count = 7
